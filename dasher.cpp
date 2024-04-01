6d6625eb5df5c84b20ce85f77aa49b5187af066d
Praful Gulani <prafulgulani555@gmail.com>
2024-04-01 15:02:52 +0530

    nebula animation

--- dasher.cpp
+++ dasher.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    //window dimesions
+    // window dimesions
     const int windowWidth{512};
     const int windowHeight{380};
     // window initialization
@@ -14,12 +14,12 @@ int main()
     // scarfy
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
-    scarfyRec.width = scarfy.width/6;
+    scarfyRec.width = scarfy.width / 6;
     scarfyRec.height = scarfy.height;
     scarfyRec.x = 0;
     scarfyRec.y = 0;
     Vector2 scarfyPos;
-    scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
+    scarfyPos.x = windowWidth / 2 - scarfyRec.width / 2;
     scarfyPos.y = windowHeight - scarfyRec.height;
     // is scarfy in the air
     bool isInAir{};
@@ -27,24 +27,31 @@ int main()
     const int jumpVel{-600};
     // scarfy y position and velocity
     int velocity{0};
-
-    // nebula 
-    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
-
-    //animation frame
+    // animation frame of scarfy
     int frame{};
     // amount of time before we update the animation frame
-    const float updateTime{1.0/12.0};
+    const float updateTime{1.0 / 12.0};
     float runningTime{};
 
+    // nebula
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+    Rectangle nebRec{0.0, 0.0, nebula.width/8, nebula.height/8};
+    Vector2 nebPos{windowWidth, windowHeight - nebRec.height};
+    // nebula x velocity in pixels/s
+    int nebVel{-200};
+    // animation frame for nebula
+    int nebFrame{};
+    const float nebUpdateTime{1.0/12.0};
+    float nebRunningTime{};
+
     SetTargetFPS(60);
-    while (!WindowShouldClose()) 
+    while (!WindowShouldClose())
     {
-        //delta time (time since last frame)
-        //delta time is time between frames and is used to keep movements consistent as frame rate affect the movements in the game
+        // delta time (time since last frame)
+        // delta time is time between frames and is used to keep movements consistent as frame rate affect the movements in the game
         const float dT{GetFrameTime()};
 
-        //start drawing
+        // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
 
@@ -68,30 +75,54 @@ int main()
             velocity += jumpVel;
         }
 
-        // update position
+        // update scarfy position
         scarfyPos.y += velocity * dT;
-        
-        // update running time
-        runningTime += dT;
-        if (runningTime >= updateTime)
+
+        // update nebula position
+        nebPos.x += nebVel * dT;
+
+        // update scarfy's animation frame
+        if (!isInAir)
         {
-            runningTime = 0.0;
-            // update animation frame
-            scarfyRec.x = frame * scarfyRec.width;
-            frame++;
-            if (frame > 5)
+            // update running time
+            runningTime += dT;
+            if (runningTime >= updateTime)
+            {
+                runningTime = 0.0;
+                // update animation frame
+                scarfyRec.x = frame * scarfyRec.width;
+                frame++;
+                if (frame > 5)
+                {
+                    frame = 0;
+                }
+            }
+        }
+
+        // update nebula's animation frame
+        nebRunningTime += dT;
+        if (nebRunningTime >= nebUpdateTime)
+        {
+            nebRunningTime = 0.0;
+            nebRec.x = nebFrame * nebRec.width;
+            nebFrame++;
+            if (frame > 7)
             {
                 frame = 0;
             }
         }
 
-
+        // draw scarfy
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+
+        // draw nebula
+        DrawTextureRec(nebula, nebRec, nebPos, WHITE);
+
         // end drawing
         EndDrawing();
     }
     UnloadTexture(scarfy);
     UnloadTexture(nebula);
-    //correct way to close the window
+    // correct way to close the window
     CloseWindow();
 }
